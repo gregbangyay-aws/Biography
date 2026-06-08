1f64d79f2cf88fb5b147b3138c31859fd26f39fd
gregbangyay-aws <greg.bangyay@awsys-i.com>
2026-06-08 14:38:19 +0800

    Updated MyBiography.java

--- MyBiography.java
+++ MyBiography.java
@@ -1,8 +1,8 @@
 public class MyBiography {
 public static void main(String[] args) {
-System.out.println("*** STUDENT BIOGRAPHY ***");
-System.out.println("Name: Greg Anthony Bangyay");
-System.out.println("Course: Information Technology");
-System.out.println("Graduation Year: 2025");
+System.out.println("***STUDENT BIOGRAPHY***");
+System.out.println("Name:" + args[0]);
+System.out.println("Major:" + args[1]);
+System.out.println("Graduation Year:" + args[2]);
 }
 }
